42a0e4047a073a84c043c5ca4d0865c5d4f579ad
sahana-sarangi <156151310+sahana-sarangi@users.noreply.github.com>
2025-11-13 01:19:24 -0800

    Update yeartoyear.py

--- yeartoyear.py
+++ yeartoyear.py
@@ -84,8 +84,8 @@ if max_abs_growth == 0 or np.isclose(max_abs_growth, 0.0):
     max_abs_growth = 1e-6
 
 color_scale = alt.Scale(
-    domain=[-max_abs_growth, -0.6 * max_abs_growth, -0.1 * max_abs_growth, 0.1 * max_abs_growth, 0.6 * max_abs_growth, max_abs_growth],
-    range=["#4575b4", "#5e4fa2", "#762a83", "#762a83", "#b2182b", "#d73027"]
+    domain=[-max_abs_growth, -0.3 * max_abs_growth, -0.05 * max_abs_growth, 0.05 * max_abs_growth, 0.3 * max_abs_growth, max_abs_growth],
+    range=["#4575b4", "#4575b4", "#762a83", "#762a83", "#d73027", "#d73027"]
 )
 
 final_chart = (
