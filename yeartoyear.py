7fa20cc76d902bd8cb6b1f2d3d3eacd403601a84
sahana-sarangi <156151310+sahana-sarangi@users.noreply.github.com>
2025-11-13 02:27:40 -0800

    Update yeartoyear.py

--- yeartoyear.py
+++ yeartoyear.py
@@ -86,7 +86,7 @@ if max_abs_growth == 0 or np.isclose(max_abs_growth, 0.0):
     max_abs_growth = 1e-6
 
 color_scale = alt.Scale(
-    domain=[-max_abs_growth, -0.105 * max_abs_growth, -0.095 * max_abs_growth, max_abs_growth],
+    domain=[-max_abs_growth, -0.101 * max_abs_growth, -0.099 * max_abs_growth, max_abs_growth],
     range=["#4575b4", "#762a83", "#762a83", "#d73027"]
 )
 
